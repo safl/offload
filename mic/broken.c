040a13d4afa4d576c5001712c7670ea33873652f
Simon A. F. Lund <safl@safl.dk>
2015-09-23 01:23:02 +0200

    ...

--- mic/broken.c
+++ mic/broken.c
@@ -108,6 +108,14 @@ void mic_pull(double* data, size_t nelem, int device, int offload)
             if (offload)
 }
 
+void mic_free(double* data, size_t nelem, int device, int offload)
+{
+    #pragma offload_transfer                                    \
+            target(mic:device)                                  \
+            nocopy(data:length(nelem) alloc_if(0) free_if(1))      \
+            if (offload)
+}
+
 void mic_pull_free(double* data, size_t nelem, int device, int offload)
 {
     #pragma offload_transfer                                    \
